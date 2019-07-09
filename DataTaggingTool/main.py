8f19ae106058c90053320575be1dcc795e37a24c
Wenenen <1042847250@qq.com>
2019-07-09 14:23:15 +0800

    完善了部分函数

--- DataTaggingTool/main.py
+++ DataTaggingTool/main.py
@@ -1,10 +1,12 @@
 import form
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 
 data_path = 'data/ '
 
-class mywindow(QtWidgets.QMainWindow,form.Ui_mainWindow):
+
+class mywindow(QtWidgets.QMainWindow, form.Ui_mainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.setupUi(self)
@@ -13,9 +15,20 @@ class mywindow(QtWidgets.QMainWindow,form.Ui_mainWindow):
         self.word_list = []
         self.label_list = []
         self.index = 0
+        self.is_save = False
+        self.yes = 1
+        self.soso = 2
+        self.no = 3
 
-    def show_word(self,index):
-        pass
+    # 在label空间上显示词
+    def show_word(self):
+        self.keyword.setText(self.word_list[self.index])
+        # self.keyword.show()
+
+    # 显示计数器
+    def show_word_cnt(self):
+        self.keyword_cnt.setText(self.index)
+        # self.keyword_cnt.show()
 
     # index增加
     def index_up(self):
@@ -23,26 +36,39 @@ class mywindow(QtWidgets.QMainWindow,form.Ui_mainWindow):
 
     # index减少
     def index_down(self):
-        self.index -= 1
+        if self.index > 0:
+            self.index -= 1
 
-    # 关键字正确，正确设置为 1
+    # 关键字正确
     def set_word_yes(self):
-        self.label_list[self.index] = 1
+        self.label_list[self.index] = self.yes
         self.index_up()
+        self.show_word()
 
-    # 关键字相似,相似设置为 0
+    # 关键字相似
     def set_word_soso(self):
-        self.label_list[self.index] = 0
+        self.label_list[self.index] = self.soso
         self.index_up()
+        self.show_word()
 
     # 关键字错误，错误设置为 -1
     def set_word_no(self):
-        self.label_list[self.index] = -1
+        self.label_list[self.index] = self.no
         self.index_up()
+        self.show_word()
 
+    # 回到上一个词
     def up_word(self):
         self.index_down()
+        self.show_word()
 
+    # 展示确定
+    def showDialog(self, message):
+        msg_box = QMessageBox(QMessageBox.Informationm, '保存成功')
+        msg_box.show()
+        msg_box.exec_()
+
+    # 保存文件
     def save_word(self):
         str = ''
         for i in range(self.index):
@@ -53,7 +79,7 @@ class mywindow(QtWidgets.QMainWindow,form.Ui_mainWindow):
         with open(self.file_name, 'w') as f:
             f.write(str)
             f.close()
-
+        self.is_save = True
 
 
 if __name__ == "__main__":
