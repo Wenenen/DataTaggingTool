79447ebec1603f88ec4d1da5651aa0324cb96b3e
Wenenen <1042847250@qq.com>
2019-07-09 10:56:40 +0800

    增加了几个变量

--- DataTaggingTool/main.py
+++ DataTaggingTool/main.py
@@ -8,7 +8,13 @@ class mywindow(QtWidgets.QMainWindow,form.Ui_mainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.setupUi(self)
+        self.file_path = ''
+        self.word_list = []
+        self.label_list = []
+        self.index = 0
 
+    def set_word_no(self):
+        print(1)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
