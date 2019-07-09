f16c26de184148ebeae68b8d0fd18c7b450ab215
Wenenen <1042847250@qq.com>
2019-07-09 17:11:45 +0800

    修改现有bug

--- DataTaggingTool/form.py
+++ DataTaggingTool/form.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'form.ui'
 #
-# Created by: PyQt5 UI code generator 5.6
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -39,19 +39,19 @@ class Ui_mainWindow(object):
         self.pushButton.setObjectName("pushButton")
         self.verticalLayout.addWidget(self.pushButton)
         self.cause_name = QtWidgets.QLabel(self.centralwidget)
-        self.cause_name.setGeometry(QtCore.QRect(40, 70, 491, 71))
+        self.cause_name.setGeometry(QtCore.QRect(40, 50, 491, 71))
         font = QtGui.QFont()
         font.setPointSize(18)
         self.cause_name.setFont(font)
         self.cause_name.setObjectName("cause_name")
         self.keyword = QtWidgets.QLabel(self.centralwidget)
-        self.keyword.setGeometry(QtCore.QRect(280, 240, 241, 51))
+        self.keyword.setGeometry(QtCore.QRect(250, 250, 281, 51))
         font = QtGui.QFont()
-        font.setPointSize(20)
+        font.setPointSize(22)
         self.keyword.setFont(font)
         self.keyword.setObjectName("keyword")
         self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(560, 70, 220, 61))
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(560, 50, 220, 61))
         self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
         self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
@@ -95,11 +95,14 @@ class Ui_mainWindow(object):
         self.no_btn.setObjectName("no_btn")
         self.horizontalLayout.addWidget(self.no_btn)
         self.backLabel = QtWidgets.QLabel(self.centralwidget)
-        self.backLabel.setGeometry(QtCore.QRect(240, 160, 311, 51))
+        self.backLabel.setGeometry(QtCore.QRect(210, 140, 491, 71))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.backLabel.setFont(font)
         self.backLabel.setObjectName("backLabel")
         mainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(mainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 793, 28))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 793, 26))
         self.menubar.setObjectName("menubar")
         mainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(mainWindow)
@@ -118,15 +121,15 @@ class Ui_mainWindow(object):
     def retranslateUi(self, mainWindow):
         _translate = QtCore.QCoreApplication.translate
         mainWindow.setWindowTitle(_translate("mainWindow", "这是一个数据标注软件"))
-        self.up_btn.setText(_translate("mainWindow", "上一个（点错时用）"))
-        self.save_btn.setText(_translate("mainWindow", "保存（退出前一定要保存！！！）"))
-        self.pushButton.setText(_translate("mainWindow", "选择文件路径"))
-        self.cause_name.setText(_translate("mainWindow", "用于显示案由"))
-        self.keyword.setText(_translate("mainWindow", "用于显示关键词"))
+        self.up_btn.setText(_translate("mainWindow", "上一个（点错时用）(u)"))
+        self.save_btn.setText(_translate("mainWindow", "保存（退出前保存!!!）(s)"))
+        self.pushButton.setText(_translate("mainWindow", "选择文件"))
+        self.cause_name.setText(_translate("mainWindow", "案由"))
+        self.keyword.setText(_translate("mainWindow", "关键词"))
         self.label_3.setText(_translate("mainWindow", "序号："))
         self.keyword_cnt.setText(_translate("mainWindow", "TextLabel"))
-        self.yes_btn.setText(_translate("mainWindow", "正确"))
-        self.soso_btn.setText(_translate("mainWindow", "相似"))
-        self.no_btn.setText(_translate("mainWindow", "错误"))
+        self.yes_btn.setText(_translate("mainWindow", "正确(1)"))
+        self.soso_btn.setText(_translate("mainWindow", "相似(2)"))
+        self.no_btn.setText(_translate("mainWindow", "错误(3)"))
         self.backLabel.setText(_translate("mainWindow", "上一步操作内容"))
 
--- DataTaggingTool/main.py
+++ DataTaggingTool/main.py
@@ -2,42 +2,53 @@ import form
 import sys
 import os
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QFileDialog,QMessageBox
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QShortcut
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtCore import Qt
 
-data_path = 'data/ '
-
+# 没有标注
+unlabel = -2
 
 class mywindow(QtWidgets.QMainWindow, form.Ui_mainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.setupUi(self)
         self.file_path = None
-        self.file_name = ''
+        self.save_file_name = ''
         self.word_list = []
-        self.label_list = [0]*500
-        self.numToLabel=["错误","相似","正确"]
+        self.label_list = []
+        self.numToLabel = ["错误", "相似", "正确"]
         self.index = 0
         self.is_save = False
         self.yes = 1
         self.soso = 0
         self.no = -1
+        self.word_list_len = 0
+        QShortcut(QKeySequence(Qt.Key_1), self, self.set_word_yes)
+        QShortcut(QKeySequence(Qt.Key_2), self, self.set_word_soso)
+        QShortcut(QKeySequence(Qt.Key_3), self, self.set_word_no)
+        QShortcut(QKeySequence(Qt.Key_U), self, self.up_word)
+        QShortcut(QKeySequence(Qt.Key_S), self, self.save_word)
 
     # 在label空间上显示词
-    def show_word(self):
-        self.keyword.setText(self.word_list[self.index])
-        # self.keyword.show()
+    # def show_word(self):
+    #     self.keyword.setText(self.word_list[self.index])
+    #     # self.keyword.show()
 
     def show_word(self):
-        if(self.index>0):
+        if self.index > 0:
             backStr="前一个的标记为\n" \
                 "序号"+str(self.index-1)+"："+self.word_list[self.index-1]+"。  标记为了"+str(self.numToLabel[self.label_list[self.index-1]+1])
             self.backLabel.setText(backStr)
         self.keyword_cnt.setText(str(self.index))
         self.keyword.setText(self.word_list[self.index])
 
-
     # index增加
     def index_up(self):
+        if self.index + 1 >= self.word_list_len:
+            msg_box = QMessageBox(QMessageBox.Information, "消息提醒", "已经是最后一个了，请保存")
+            msg_box.exec_()
+            return
         self.index += 1
 
     # index减少
@@ -45,75 +56,119 @@ class mywindow(QtWidgets.QMainWindow, form.Ui_mainWindow):
         if self.index > 0:
             self.index -= 1
 
+    # 文件已经被改变
+    def is_change(self):
+        self.is_save = False
+
     # 关键字正确
     def set_word_yes(self):
+        if self.file_path is None:
+            return
         self.label_list[self.index] = self.yes
         self.index_up()
         self.show_word()
+        self.is_change()
 
     # 关键字相似
     def set_word_soso(self):
+        if self.file_path is None:
+            return
         self.label_list[self.index] = self.soso
         self.index_up()
         self.show_word()
+        self.is_change()
 
-    # 关键字错误，错误设置为 -1
+    # 关键字错误
     def set_word_no(self):
+        if self.file_path is None:
+            return
         self.label_list[self.index] = self.no
         self.index_up()
         self.show_word()
+        self.is_change()
 
-    # 上一步操作，供误操作使用
     # 回到上一个词
-
     def up_word(self):
+        if self.file_path is None:
+            return
         self.index_down()
         self.show_word()
 
-    # 展示确定
-    def showDialog(self, message):
-        msg_box = QMessageBox(QMessageBox.Informationm, '保存成功')
-        msg_box.show()
-        msg_box.exec_()
-
     # 保存文件
     def save_word(self):
-        str = ''
-        for i in range(self.index):
-            str += self.word_list[i]
-            str += ' '
-            str += self.label_list[i]
-            str += '\n'
-        with open(self.file_name, 'w') as f:
+        if self.file_path is None:
+            return
+        tmpstr = ''
+        # 保存所有词
+        cnt = 0
+        for word in self.word_list:
+            tmpstr += word
+            tmpstr += ' '
+            tmpstr += str(self.label_list[cnt])
+            tmpstr += '\n'
+            cnt += 1
+
+        with open(self.save_file_name, 'w', encoding="utf-8") as f:
             # for word,label in zip(word_list,label_list):
             #     f.write(str(word)+" "+str(label)+"\n")
-            f.write(str)
-            f.close()
+            f.write(tmpstr)
 
-        #清空相关变量
-        self.file_name=None
-        self.file_path=None
-        self.word_list.clear()
-        self.label_list = [0] * 500
         self.is_save = True
-
+        msg_box = QMessageBox(QMessageBox.Information, "消息提醒", "保存成功")
+        msg_box.exec_()
 
     def selectTxtFilePath(self):
+        # 清空相关变量
+        # self.file_name = None
+        # self.file_path = None
+        # self.word_list.clear()
+        # self.label_list = []
+        # self.is_save = True
+        # self.index = 0
         self.file_path = QFileDialog.getOpenFileName(self, 'Open file', os.getcwd())
-        if self.file_path[0]:
+        if self.file_path[0]:  # 如果有这个文件
             self.cause_name.setText(os.path.split(self.file_path[0])[1])
-            self.file_name = self.file_path[0]+"_label"
-            with open(self.file_path[0], "r") as txtFile:
-                content = txtFile.readline()
-                while(content):
-                    content = content.split()
-                    self.word_list.append(content[0])
-                    content = txtFile.readline()
-            self.keyword.setText(self.word_list[0])
+            self.save_file_name = self.file_path[0]+"_label"
+
+            with open(self.file_path[0], "r", encoding='utf-8') as txtFile:
+                lines = txtFile.readlines()
+                for line in lines:
+                    line = line.split()
+                    self.word_list.append(line[0])
+                # 获取label_list
+                self.label_list = [unlabel]*len(self.word_list)
+
+            # 如果已经标注过,那么读取里面的标签
+            if os.path.exists(self.save_file_name):
+                with open(self.save_file_name, 'r', encoding='utf-8') as save_file:
+                    index = 0
+                    lines = save_file.readlines()
+                    for line in lines:
+                        line = line.split()
+                        label = line[1]
+                        # 如果没有标注
+                        if unlabel == int(label):
+                            self.index = index
+                            break
+                        self.label_list[index] = int(label)
+                        index += 1
+            self.keyword.setText(str(self.word_list[0]))
             self.keyword_cnt.setText("0")
+            self.word_list_len = len(self.word_list)
+            self.show_word()
         else:
             msg_box = QMessageBox(QMessageBox.Information, "消息提醒", "好像没选数据文件噢")
             msg_box.exec_()
+
+    def closeEvent(self, event):
+        if self.is_save:
+            return
+        reply = QtWidgets.QMessageBox.question(self, '警告', '还未保存，确定退出？',
+                                           QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
